24351abeebfaf9d118f30a07d08d31dc3496fa81
jeckscolari <giacomo.scolari92@gmail.com>
2021-11-25 17:20:11 +0100

    rename app

--- bin/app.ts
+++ bin/app.ts
@@ -1,14 +1,13 @@
-import * as cdk from '@aws-cdk/core';
-import { Stack } from '../lib/stack';
+import * as cdk from "@aws-cdk/core";
+import { Stack } from "../lib/stack";
 
 const app = new cdk.App();
 
 const env = {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
 };
 
-
-const stack = new Stack(app, 'GraphqlQldbPlayground', {
-    env
+const stack = new Stack(app, "AppsyncQldbPlayground", {
+  env,
 });
